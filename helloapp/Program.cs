76a79e79b87f8c8f21f3f01dbd24e3f833c96118
PorubaimikhYehor <porubaimikh.yehor@gmail.com>
2020-03-16 01:58:36 +0200

    first

---
+++ helloapp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace helloapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+            // Console
+        }
+    }
+}
